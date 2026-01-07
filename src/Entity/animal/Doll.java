7bc4e905f17e79bdeadcd0e9fee06cfc6bf039a2
Duong Hoang <h1tm4ni8@gmail.com>
2026-01-07 18:08:12 +0700

    update moveDoll (nó sẽ chỉ tính sau khi player di chuyển, hoặc sau mỗi 5 lượt di chuyển)

--- src/Entity/animal/Doll.java
+++ src/Entity/animal/Doll.java
@@ -1,27 +1,28 @@
 package Entity.animal;
 
+import java.util.List;
+
 import Control.Move;
 import Entity.animal.intelligent.AStar;
 import Entity.animal.intelligent.Node;
 import Graphics.Sprite;
 import javafx.scene.image.Image;
 
-import java.util.List;
-
-import static GameRunner.RunBomberman.enemy;
-import static GameRunner.RunBomberman.height;
-import static GameRunner.RunBomberman.id_objects;
-import static GameRunner.RunBomberman.player;
-import static GameRunner.RunBomberman.width;
+import static GameRunner.RunBomberman.*;
 
 public class Doll extends Animal{
     private static int swap_kill = 1;
     private static int count_kill = 0;
 
+    // Biến để tính việc tính lại A*
+    private int pathCooldown = 0;
+    private List<Node> currentPath = null;
+    private int lastPlayerTileX = -1;
+    private int lastPlayerTileY = -1;
+
     public Doll(int x_unit, int y_unit, Image img) {
         super(x_unit, y_unit, img);
     }
-
 
     public Doll(boolean life) {
         super(life);
@@ -47,41 +48,70 @@ public class Doll extends Animal{
         }
     }
 
-    private void moveDoll() {
-        if (this.x % 32 == 0 && this.y %32 == 0) {
-            Node initial_node = new Node(this.y / 32, this.x / 32);
-            Node final_node = new Node(player.getY() / 32, player.getX() / 32);
-
-            int rows = height;
-            int cols = width;
-
-            AStar a_star = new AStar(rows, cols, initial_node, final_node);
-
-            int[][] blocks_in_array = new int[width * height][2];
-            int count_block = 0;
-
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    if (id_objects[j][i] != 0) {
-                        blocks_in_array[count_block][0] = i;
-                        blocks_in_array[count_block][1] = j;
-                        count_block++;
-                    }
+    private void recomputePath() {
+        Node initialNode = new Node(getY() / 32, getX() / 32);
+        Node finalNode = new Node(player.getY() / 32, player.getX() / 32);
+        int rows = height;
+        int cols = width;
+        AStar a_star = new AStar(rows, cols, initialNode, finalNode);
+        int[][] blocks_in_array = new int[width * height][2];
+        int count_block = 0;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (id_objects[j][i] != 0) {
+                    blocks_in_array[count_block][0] = i;
+                    blocks_in_array[count_block][1] = j;
+                    count_block++;
                 }
             }
-            a_star.setBlocks(blocks_in_array, count_block);
-            List<Node> path = a_star.findPath();
-            if (path.size() != 0) {
-                int nextX = path.get(1).getCol();
-                int nextY = path.get(1).getRow();
+        }
+        a_star.setBlocks(blocks_in_array, count_block);
+        currentPath = a_star.findPath();
 
-                if (this.y / 32 > nextY)
+        // Store player position for change detection
+        lastPlayerTileX = player.getX() / 32;
+        lastPlayerTileY = player.getY() / 32;
+
+        // Reset cooldown to recompute after N tile movements
+        pathCooldown = 5; // Adjust this value: higher = less frequent recomputation
+    }
+    private boolean shouldRecomputePath() {
+        // Recompute if no path exists
+        if (currentPath == null || currentPath.size() < 2) {
+            return true;
+        }
+
+        // Recompute if cooldown expired
+        if (pathCooldown <= 0) {
+            return true;
+        }
+
+        // Recompute if player moved to a different tile
+        int currentPlayerTileX = player.getX() / 32;
+        int currentPlayerTileY = player.getY() / 32;
+        return currentPlayerTileX != lastPlayerTileX || currentPlayerTileY != lastPlayerTileY;
+    }
+
+
+    private void moveDoll() {
+        if (getX() % 32 == 0 && getY() % 32 == 0) {
+            // Check if we need to recompute the path
+            if (shouldRecomputePath()) {
+                recomputePath();
+            } else {
+                pathCooldown--;
+            }
+            // Follow the cached path
+            if (currentPath != null && currentPath.size() >= 2) {
+                int nextX = currentPath.get(1).getCol();
+                int nextY = currentPath.get(1).getRow();
+                if (getY() / 32 > nextY)
                     Move.up(this);
-                if (this.y / 32 < nextY)
+                if (getY() / 32 < nextY)
                     Move.down(this);
-                if (this.x / 32 > nextX)
+                if (getX() / 32 > nextX)
                     Move.left(this);
-                if (this.x / 32 < nextX)
+                if (getX() / 32 < nextX)
                     Move.right(this);
             }
         }
