eb2340e5eb4de7913df8ce7f56d74b2e47b0ae77
longtran9703-hash <longtran9703@gmail.com>
2026-01-09 09:55:26 +0700

    Fix map file path for Level2 initialization

--- src/main/java/levels/Level2.java
+++ src/main/java/levels/Level2.java
@@ -23,7 +23,7 @@ public class Level2 {
         enemy.clear();
         block.clear();
         swap_kill = 1;
-        new MapCreation("/levels/level2.txt");
+        new MapCreation("/levels/Level2.txt");
         player.setLife(true);
         player.setX(32);
         player.setY(32);
