640947746e94b67ffa23e7c1c8f2ed812b9a5e10
longtran9703-hash <longtran9703@gmail.com>
2026-01-09 09:46:05 +0700

    Add InputStream import to MapCreation.java

--- src/main/java/Graphics/MapCreation.java
+++ src/main/java/Graphics/MapCreation.java
@@ -14,6 +14,7 @@ import GameRunner.RunBomberman;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
