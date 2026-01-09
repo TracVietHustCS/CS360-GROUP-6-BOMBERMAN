7ae49b3539745ff6e11cf44bd8f94a20e9e0da25
longtran9703-hash <longtran9703@gmail.com>
2026-01-09 08:10:04 +0700

    Rename Levels to levels (lowercase L) as the resources would be packed in the same place

--- src/levels/Level2.java
+++ src/levels/Level2.java
@@ -1,4 +1,4 @@
-package Levels;
+package levels;
 
 import Entity.animal.Animal;
 import Entity.animal.Ballom;
@@ -23,7 +23,7 @@ public class Level2 {
         enemy.clear();
         block.clear();
         swap_kill = 1;
-        new MapCreation("resources/levels/level2.txt");
+        new MapCreation("/levels/level2.txt");
         player.setLife(true);
         player.setX(32);
         player.setY(32);
